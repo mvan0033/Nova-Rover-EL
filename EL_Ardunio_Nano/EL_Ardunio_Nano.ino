8bf20c14e59b5b2f8a9bf0df8315d4bac68df005
Matt van Wijk <mvan0033@student.monash.edu>
2021-03-28 02:26:18 +1100

    LCD UI updates

--- EL_Ardunio_Nano/EL_Ardunio_Nano.ino
+++ EL_Ardunio_Nano/EL_Ardunio_Nano.ino
@@ -1,65 +1,172 @@
 // Importing
-#include <LiquidCrystal_I2C.h>
+#include "LiquidCrystal_I2C.h"
 #include <Wire.h>
 
 // Global variables
+#define rotary_encoder_clk 2
+#define rotary_encoder_dt 3
+#define rotary_encoder_sw 4
+volatile bool clockwise;
+volatile bool turnDetected = false;
+volatile bool buttonPressed = false;
 float power = 0;
 float current = 0;
 float curCurrent = 0;
 float curPower = 0;
 float lowVolt = 0;
 float hotTemp = 0;
-int elaspsedTime = 0;
+int elapsedTime = 0;
 int lcdAddress = 0x27;
+int screen = 1;
+volatile bool powerMode = false;
+int cursorPosition = 0;
+
+
+LiquidCrystal_I2C lcd(lcdAddress, 128, 64);
 
 // Custom LCD Characters
-byte rightArrow[8] = 
-{
-  0b10000,
-  0b11000,
-  0b11100,
-  0b11110,
-  0b11110,
-  0b11100,
-  0b11000,
-  0b10000
-};
+//byte rightArrow[8] = 
+//{
+//  0b10000,
+//  0b11000,
+//  0b11100,
+//  0b11110,
+//  0b11110,
+//  0b11100,
+//  0b11000,
+//  0b10000
+//};
 
-byte upDownArrow[8] = 
+void isr0()
 {
-  0b00100,
-  0b01110,
-  0b11111,
-  0b00000,
-  0b00000,
-  0b11111,
-  0b01110,
-  0b00100,
-};
+  turnDetected = true;
+  clockwise = digitalRead(rotary_encoder_clk) != digitalRead(rotary_encoder_dt);
+    Serial.print("\nTurn detected!\n");
+//  Serial.print(clockwise);
+}
+
+ISR(PCINT2_vect) {
+  if (digitalRead(rotary_encoder_sw) == LOW) 
+  {
+    buttonPressed = true;
+    Serial.print("\nbutton pressed!\n");
+   }
+}
+  
 
 void setup()
 {
   // put your setup code here, to run once:
- LiquidCrystal_I2C lcd(lcdAdd, 128, 64);
- lcd.begin();
- lcd.createChar(0, rightArrow); 
- lcd.createChar(1, upDownArrow);
+ lcd.init();
+ lcd.backlight();
+ //lcd.createChar(0, rightArrow); 
+ //lcd.createChar(0, upDownArrow);
+ //screen1();
+ //lcd.setCursor(0, 0);
+ //lcd.write(byte(1)); // 0,2,4,6
+ //lcd.write(byte(5));
+ pinMode(rotary_encoder_clk, INPUT);
+ pinMode(rotary_encoder_dt, INPUT);
+ pinMode(rotary_encoder_sw, INPUT);
+ PCICR |= 0b00000100;
+ PCMSK2 |= 0b00010000;   // turn o PCINT20(D4)
+ Serial.begin(9600);
+ attachInterrupt(digitalPinToInterrupt(rotary_encoder_dt), isr0, RISING);
+ screen0();
+ delay(2500);
+ screen1();
+ lcd.setCursor(1,0);
+ lcd.write(byte(62));
 }
 
 void loop() 
 {
-  // put your main code here, to run repeatedly:
+  if (turnDetected)
+  {
+    delay(200);
+    switch (screen)
+    {
+      case 1:
+      {
+        if (clockwise)
+        {
+          lcd.setCursor(1,0);
+          lcd.write(byte(32));
+          lcd.setCursor(2,0);
+          lcd.write(byte(62));
+          powerMode = false;
+        }
+        else
+        {
+          lcd.setCursor(2,0);
+          lcd.write(byte(32));
+          lcd.setCursor(1,0);
+          lcd.write(byte(62));
+          powerMode = true;
+        }
+        break;
+       case 2:
+       {
+          lcd.setCursor(cursorPosition, 0);
+          lcd.write(byte(32));
+          
+          if (clockwise)
+          { 
+              cursorPosition = (cursorPosition + 1) % 3;
+          }
+          else
+          {
+              cursorPosition = (cursorPosition - 1) % 3;
+              cursorPosition = cursorPosition < 0 ? 3 + cursorPosition : cursorPosition;
+          }
+          lcd.setCursor(cursorPosition, 0);
+          lcd.write(byte(62));
+       }
+       break;
+      }
+    }
+    turnDetected = false;
+  }
 
+  if (buttonPressed)
+  {
+    delay(200);
+    switch (screen)
+    {
+      case 1:
+      {
+        if (powerMode)
+        {
+          screen3();
+          screen = 3;
+        }
+        else
+        {
+          screen2();
+          screen = 2;
+        }
+      }
+      break;
+      case 2:{}
+      case 3:{}
+      case 4:{}
+      case 5:{}
+      case 6:{}
+      case 7:{}
+    }
+  }
 }
 
 void screen0() 
 {
   lcd.clear();
-  lcd.setCursor(1, 0);
+  lcd.setCursor(0, 0); // Start of 1st row
   lcd.print("MONASH NOVA");
-  lcd.setCursor(3, 1);
+  lcd.setCursor(1, 0); // Start of 2nd row
   lcd.print("ROVER");
-  lcd.setCursor(0,3);
+//  lcd.setCursor(24, 0);
+//  lcd.setCursor(2, 10);
+  lcd.setCursor(2, 0); // Start of 3rd row
   lcd.print("ELECTRONIC LOAD");
 }
 
@@ -68,21 +175,24 @@ void screen1()
   lcd.clear();
   lcd.setCursor(1, 1);
   lcd.print("Current Mode");
-  lcd.setCursor(1, 2);
+  lcd.setCursor(2, 1);
   lcd.print("Power Mode");
 }
 
 void screen2()
 {
   lcd.clear();
-  lcd.setCursor(1, 0);
+  lcd.setCursor(0, 1);
   lcd.print("Current:");
   lcd.print(current);
+  lcd.setCursor(0, 7);
   lcd.print("A");
-  lcd.print(1, 1);
+  lcd.setCursor(1, 1);
   lcd.print("Next");
-  lcd.setCursor(1, 2);
+  lcd.setCursor(2, 1);
   lcd.print("Back");
+  lcd.setCursor(0,0);
+  lcd.write(byte(62));
 }
 
 void screen3()
@@ -90,9 +200,10 @@ void screen3()
   lcd.clear();
   lcd.setCursor(1, 0);
   lcd.print("Power:");
+  lcd.setCursor(1, 7);
   lcd.print(power);
   lcd.print("W");
-  lcd.print(1, 1);
+  lcd.setCursor(2, 1);
   lcd.print("Next");
   lcd.setCursor(1, 2);
   lcd.print("Back");
@@ -240,6 +351,6 @@ void screen14()
   lcd.print("ANALYSIS");
   lcd.setCursor(2, 1);
   lcd.print("COMPLETE");
-  lcd.setCursor(0, 2);
+  lcd.setCursor(0,0);
   lcd.print("REMOVE SD CARD");
 }
