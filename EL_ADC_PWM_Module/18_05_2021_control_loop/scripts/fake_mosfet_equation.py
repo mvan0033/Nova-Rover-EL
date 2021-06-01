1f151cb8f1785f0c15f28117e93b6bd76a0d4ecb
Leigh Oliver <leighcharlesoliver@gmail.com>
2021-06-01 17:33:47 +1000

    Added fake mosfet equation python script

--- EL_ADC_PWM_Module/18_05_2021_control_loop/scripts/fake_mosfet_equation.py
+++ EL_ADC_PWM_Module/18_05_2021_control_loop/scripts/fake_mosfet_equation.py
@@ -7,7 +7,7 @@ eq1_current = lambda x: (2.5923060142355E-15) * math.exp(8.1407449923113 * x)
 eq2_current = lambda x: 734.04065811719 * math.pow(x,2) - 5824.45578150388 * x + 11554.22728209
 fake_mos = lambda x: eq1_current(x) if x < 3.96739 else eq2_current(x)
 
-v_gs = np.linspace(3.4,4.0,1000)
+v_gs = np.linspace(4.0,4.1,math.floor(65535 * (0.1/5)))
 fake_mos_data = [fake_mos(x) for x in v_gs]
 plt.plot(v_gs,fake_mos_data)
 plt.show()
